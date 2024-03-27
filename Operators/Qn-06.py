33df9cfa30185838fd42afac833e8451c275d176
Asif Ali Sahaji <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2024-03-27 21:00:44 +0530

    Create Qn-06.py

---
+++ Operators/Qn-06.py
@@ -0,0 +1,7 @@
+#write a python script to calculate area of a triangle. length of sides are given by user
+
+h=input("Enter the height of the triangle:")
+b=input("Enter the base of the triangle:")
+
+a=int(h)*int(b)/2
+print("The area of triangle is:",a)
