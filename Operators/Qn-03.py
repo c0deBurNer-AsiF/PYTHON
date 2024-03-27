7b482b62d3547a058aa4f54b3ee11f5f4429f3b8
Asif Ali Sahaji <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2024-03-27 13:23:02 +0530

    Create Qn-03.py

---
+++ Operators/Qn-03.py
@@ -0,0 +1,9 @@
+#write a python script to calculate the volume of cubiod , dimension(float value)taken from user
+
+l=input("Enter the length of the cubiod:")
+b=input("Enter the breadth of the cubiod:")
+h=input("Enter the height of the cubiod:")
+
+v=int(l)*int(b)*int(h)
+
+print("The volume is:",v)
