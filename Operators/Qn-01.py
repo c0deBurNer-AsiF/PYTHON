a46b9cda325ecd6be5b10011f6f713ccc5264f2b
Asif Ali Sahaji <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2024-03-27 13:05:55 +0530

    Create Qn-01.py

---
+++ Operators/Qn-01.py
@@ -0,0 +1,8 @@
+#Write a python script to add two numbers(integer) taken from user through keyboard
+
+
+x=input("Enter first number:")
+y=input("Enter second numbers:")
+
+print("Sum is:",int(x)+int(y))
+
