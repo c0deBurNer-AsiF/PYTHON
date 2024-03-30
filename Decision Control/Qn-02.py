772897dac68f08fd88d42c387b260da72e61b130
Asif Ali Sahaji <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2024-03-30 10:54:56 +0530

    Create Qn-02.py

---
+++ Decision Control/Qn-02.py
@@ -0,0 +1,9 @@
+#write a python script to check whether a given number is divisible by 5 or not
+
+x=int(input("Enter a number"))
+
+if x%5==0:
+      print("Its divisible by 5")
+
+else:
+    print("Its not divisible")
