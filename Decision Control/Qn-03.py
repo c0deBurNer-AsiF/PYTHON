dbb1b639b6b43a1c0acd2f4e0799fc319f4dc9d7
Asif Ali Sahaji <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2024-03-30 11:01:27 +0530

    Create Qn-03.py

---
+++ Decision Control/Qn-03.py
@@ -0,0 +1,15 @@
+#write a python script to check whether a given number is positive, negative or zero
+
+x=int(input("Enter a number"))
+
+if x>0:
+    print("Its a positive number")
+
+elif x<0:
+    print("Its a negative number")
+
+else:
+    print("Zero")
+    
+
+
