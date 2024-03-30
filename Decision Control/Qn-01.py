8c342734521b612f2c8c40b1e4fcbfe12e44524b
Asif Ali Sahaji <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2024-03-30 10:48:56 +0530

    Create Qn-01.py

---
+++ Decision Control/Qn-01.py
@@ -0,0 +1,12 @@
+#write a python script to check whether a given number is even or odd
+
+
+x=int(input("enter a number:"))
+if x%2==0 :
+    print("Its a even number")
+
+elif x%2!=0:
+    print("Its a odd number")
+
+else:
+    print("Invalid number")
