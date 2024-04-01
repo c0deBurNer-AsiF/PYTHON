8599f64c6f3d48da1eeb38d6604e2156a8c05e26
Asif Ali Sahaji <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2024-04-01 13:03:26 +0530

    Create Qn-05.py

---
+++ while loop/Qn-05.py
@@ -0,0 +1,8 @@
+#write a python script to print first N natural numbers. value of N taken from user
+
+x=int(input("Enter how many natural numbers u want to print:"))
+i=1
+while i<=x:
+    print(i)
+    i+=1
+
