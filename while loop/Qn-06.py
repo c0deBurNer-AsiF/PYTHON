a68d6d630d0a7bf2b614a3f6d9023e7dc6c304ee
Asif Ali Sahaji <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2024-04-01 13:09:19 +0530

    Create Qn-06.py

---
+++ while loop/Qn-06.py
@@ -0,0 +1,8 @@
+#write a python script to print first N natural numbers in reverse order. value of N taken from user
+
+x=int(input("Enter how many natural numbers u want to print:"))
+
+while x>=1:
+    print(x)
+    x-=1
+
