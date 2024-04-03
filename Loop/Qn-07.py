f220e8bc1484da03983fa6e918d6a277c1e82c22
Asif Ali Sahaji <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2024-04-03 23:41:54 +0530

    Create Qn-07.py

---
+++ Loop/Qn-07.py
@@ -0,0 +1,7 @@
+#write a python script to print first N even natural numbers in reverse order using range function in for loop.
+
+x=int(input("Enter a how many even number u want to print in reverse order"))
+for e in range(x*2,0,-2):
+    print(e)
+    
+          
