ff2577b9584339e1188ad5959cea2930956c8ecb
Asif Ali Sahaji <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2024-04-09 14:24:33 +0530

    Update Qn-02.py

--- List/Qn-02.py
+++ List/Qn-02.py
@@ -4,3 +4,9 @@ l=[int(e) for e in input("Enter any number which are separated by comma").split(
 l.sort()
 l.reverse()
 print("Greatest number is",l[0])
+
+#2nd method--->
+'''
+l=[int(x) for x in input("enter numbers separated by comma:").split(',')]
+print("Maximum number of this list is:",max(l))
+'''
