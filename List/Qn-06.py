e7d5a47f986b0a6fd00db794c4170e2437969ca6
Asif Ali Sahaji <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2024-04-09 12:28:12 +0530

    Update Qn-06.py

--- List/Qn-06.py
+++ List/Qn-06.py
@@ -2,12 +2,15 @@
 
 l=[int(a)for a in input("Enter numbers which are separeted with comma").split(',')]
 print(l)
+s=0
 n=int(input("Enter any number from this list"))
-i=0
 for e in l:
     if e==n:
-        print(l.index(e))
+        s=l.index(e)
+        print(s,end=" ")
         l.remove(e)
+        l.insert(s,0)
+
 
 #2nd method---->
 '''
