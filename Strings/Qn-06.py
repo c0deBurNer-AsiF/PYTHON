d0b4a37f0cb5fdef10346c8df8ee0bf532deb666
Asif Ali Sahaji <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2024-04-13 01:11:21 +0530

    Create Qn-06.py

---
+++ Strings/Qn-06.py
@@ -0,0 +1,13 @@
+#write a python script to arrange words in a given string in alphabetical order
+
+for e in sorted(input("Enter strings").split(' ')):
+    print(e,end=' ')
+
+
+#2nd method---->
+'''
+l=input("Enter strings:").split(' ')
+for e in sorted(l):
+    print(e)
+    
+'''
