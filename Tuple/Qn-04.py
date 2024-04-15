6863ca4147a8be25e2c137ed29be0a479e1ef764
Asif Ali Sahaji <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2024-04-16 01:11:09 +0530

    Update Qn-04.py

--- Tuple/Qn-04.py
+++ Tuple/Qn-04.py
@@ -2,3 +2,15 @@
 
 print("Reverse tuple is",tuple(reversed(eval(input("Enter a tuple:")))))
 
+
+
+
+#2nd Method--->
+'''
+t=eval(input("Enter a tuple:"))
+print("User entered tuple is:",t)
+l=list(t)
+l.reverse()
+t=tuple(l)
+print("Reverse tuple is:",t)
+'''
