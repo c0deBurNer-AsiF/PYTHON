1af10c168d02be7ca351b79b818eb6352df4ee41
Asif Ali Sahaji <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2024-04-16 22:48:31 +0530

    Create Qn-06.py

---
+++ Tuple/Qn-06.py
@@ -0,0 +1,8 @@
+'''write a python script to calculate sum of the elements of the tuple.
+tuple input taken from keyboard(all values are of int type)'''
+
+
+s=0
+for i in eval(input("Enter tuples:")):
+    s=s+i
+print("Sum of the elements is-->",s)
