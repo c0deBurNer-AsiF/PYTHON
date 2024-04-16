5abb5d8654d5c03229e349adb90ea6752aa2ad5f
Asif Ali Sahaji <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2024-04-17 00:38:35 +0530

    Create Qn-09.py

---
+++ Tuple/Qn-09.py
@@ -0,0 +1,5 @@
+#write a python script to compare two tuples, whether they contain the same elements in same order or not
+
+t1=eval(input("Enter first tuple:"))
+t2=eval(input("Enter second tuple:"))
+print("Same order") if t1==t2 else print("not same order")
