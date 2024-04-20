471779b0035cddc8ebab8de9779135fe388101c2
Asif Ali Sahaji <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2024-04-21 00:34:29 +0530

    Update Qn-02.py

--- Dict/Qn-02.py
+++ Dict/Qn-02.py
@@ -1,10 +1,17 @@
 #Write a python script to create a dictionary in which each item is a pair of roll number (as KEY) and student name (as VALUE)
 
+print("Enter rollno and name like rollno:name,rollno:name..:")
+d={int(e[0:e.index(':'):]) : e[e.index(':')+1::] for e in input().split(',')}
+print(d)
 
 
+
+#2nd method
+'''
 words={}
 l=list(input("Enter Roll number:").split(','))
 for i in l:
    print("Enter name of this roll number:",i)
    words[i]=input()
 print(words)
+'''
