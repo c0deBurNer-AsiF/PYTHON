1dc88113b7036940407b9c50e6b9edb1448ea79c
Asif Ali Sahaji <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2024-04-21 11:22:40 +0530

    Update Qn-03.py

--- Dict/Qn-03.py
+++ Dict/Qn-03.py
@@ -4,3 +4,17 @@ print("Enter rollno and name like this rollno:name,rollno:name....:")
 d={int(e[0:e.index(':'):]):e[e.index(':')+1::] for e in input().split(',')}
 for k in sorted(d):
    print(k,'---',d[k])
+
+
+
+#2nd type---->>
+
+'''
+words={}
+l=list(input("Enter character as a key:").split(','))
+for i in l:
+   print("Enter name of this roll number:",i)
+   words[i]=input()
+for i,k in sorted(words.items()):
+   print(i,'---',k)
+'''
