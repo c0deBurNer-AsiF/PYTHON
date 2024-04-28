161223b3121c4c08796089f676fcebf398e48fff
Asif Ali Sahaji <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2024-04-29 00:47:08 +0530

    Update Qn-01.py

--- Recurssion/Qn-01.py
+++ Recurssion/Qn-01.py
@@ -1,7 +1,6 @@
 #Write a recursive function to print first N natural numbers.
 
 def natural(n):
-    if n==0:
-        return 0
-    natural(n-1)
-    print(n,end=' ')
+    if n>0:
+        natural(n-1)
+        print(n,end=' ')
