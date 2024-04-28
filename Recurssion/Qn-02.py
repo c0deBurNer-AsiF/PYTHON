2db3cfe4a481e6aba550f171ea33ecb4170a6af5
Asif Ali Sahaji <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2024-04-29 00:46:34 +0530

    Update Qn-02.py

--- Recurssion/Qn-02.py
+++ Recurssion/Qn-02.py
@@ -1,9 +1,8 @@
 #Write a recursive function to print first N natural numbers in reverse order.
 
 def revntrl(n):
-    if n==0:
-        return 0
-    print(n,end=' ')
-    revntrl(n-1)
+    if n>0:
+        print(n,end=' ')
+        revntrl(n-1)
 
            
