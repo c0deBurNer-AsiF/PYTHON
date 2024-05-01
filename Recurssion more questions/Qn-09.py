1930a432a57099e56b89f76717d08d8eda5b3819
Asif Ali Sahaji <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2024-05-01 13:22:14 +0530

    Update Qn-09.py

--- Recurssion more questions/Qn-09.py
+++ Recurssion more questions/Qn-09.py
@@ -7,4 +7,16 @@ def fibo(n,x=1,y=0):
         z=x+y
         print(z,end=' ')
         fibo(n=n-1,x=y,y=z)
-        
+#output: 1 1 2 3 5 8.......
+
+'''
+def fibo(n,x=-1,y=1):
+    if n>0:
+        z=0
+        z=x+y
+        print(z,end=' ')
+        fibo(n=n-1,x=y,y=z)
+
+    
+'''
+#output: 0 1 1 2 3 5 8......
