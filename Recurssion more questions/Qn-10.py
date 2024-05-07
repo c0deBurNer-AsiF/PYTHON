b2bf857a747960f9f1211a7ecaf316e93332fe9d
Asif Ali Sahaji <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2024-05-07 10:13:23 +0530

    Update Qn-10.py

--- Recurssion more questions/Qn-10.py
+++ Recurssion more questions/Qn-10.py
@@ -2,10 +2,7 @@
 
 
 def hcf(a,b):
-    x,y=a,b
-    i=a if a<b else b
-    if x%i==0 and y%i==0:
-        return i
-    return hcf(a=a-1,b=b-1)
-    
+    if a<b:
+        a,b=b,a
+    return (b if a%b==0 else hcf(a%b,b))  
             
