361270d4a7c49151c5d2031f52b393dd53fbe46e
Asif Ali Sahaji <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2024-07-04 15:17:00 +0530

    Update Qn-04.py

--- Decision Control/Qn-04.py
+++ Decision Control/Qn-04.py
@@ -1,5 +1,29 @@
 #write a python script to find greatest among three numbers
 
+x,y,z=int(input("Enter first number:")),int(input("Enter second number:")),int(input("Enter the thired number:"))
+if x>y:
+    print(x if x>z else z)
+else:
+    print(y if y>z else z)    
+
+
+#2nd method--->
+'''
+x,y,z=int(input("Enter first number:")),int(input("Enter second number:")),int(input("Enter the thired number:"))
+if x>=y>=z or x>=z>=y:
+    print(x)
+elif y>=x>=z or y>=z>=x:
+    print(y)
+elif z>=x>=y or z>=y>=x:
+    print(z)
+else:
+    print(x)
+'''    
+
+
+
+#3rd method--->
+'''
 x=int(input("Enter 1st number"))
 y=int(input("Enter 2nd number"))
 z=int(input("Enter 3rd number"))
@@ -12,5 +36,5 @@ elif y>x and y>z:
 
 elif z>x and z>y :
     print("Greatest number is %d"%z)
-
+'''
 
