d71022279e8963c2695db08d9c67d94a72d969de
Asif Ali Sahaji <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2024-07-04 15:13:32 +0530

    Update Qn-05.py

--- Decision Control/Qn-05.py
+++ Decision Control/Qn-05.py
@@ -1,5 +1,22 @@
 #write a python script to check if a year is leap year or not
 
+
+x=input("Enter a year to check its leap year or not:")
+if x[-1]=='0' and x[-2]=='0':
+    if int(x)%400==0:
+        print("Its a leap year")
+    else:
+        print("Its not a leap year")
+else:
+    if int(x)%4==0:
+        print("Its a leap year")
+    else:
+        print("Its not a leap year") 
+
+
+
+#2nd method--->
+'''
 x=int(input("Enter a year"))
 
 if x%100==0:
@@ -14,3 +31,4 @@ elif x%100!=0:
     else:
         print("Its not a leap year")
     
+'''
