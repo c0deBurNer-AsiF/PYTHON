1b5d68529a0aa728376a84442f6b768d337c3447
Asif Ali Sahaji <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2024-07-05 22:45:15 +0530

    Create Qn-03.py

---
+++ for loop/Qn-03.py
@@ -0,0 +1,18 @@
+#count occurance of spaces in a given string
+
+print('Total number of spaces-->',input("Enter a string:").count(" "))
+
+#2nd method-->
+'''
+x=input("Enter a string:")
+print('Total number of spaces-->',x.count(" "))
+'''
+
+#3nd method--->
+'''
+count=0
+for i in input("Enter a string:"):
+    if i==" ":
+        count+=1
+print("Total number of spaces in this string is-->",count)
+'''
