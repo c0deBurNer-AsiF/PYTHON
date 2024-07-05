ecaaee08ecff42f7ec3bbfa16ba93b3d7c3b4a60
Asif Ali Sahaji <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2024-07-05 14:38:44 +0530

    Create Qn-15.py

---
+++ while loop/Qn-15.py
@@ -0,0 +1,7 @@
+#write a python script to print square of first N natural number
+x=int(input("Enter how many natural number of squares you want to print:"))
+i=1
+while i<=x:
+    print(i**2,end=' ') 
+    i+=1
+
