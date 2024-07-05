86674f2ed8396ceafc4d2ea27aed0cc69f1b4063
Asif Ali Sahaji <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2024-07-05 14:19:35 +0530

    Create Qn-11.py

---
+++ while loop/Qn-11.py
@@ -0,0 +1,7 @@
+#write a python script to print square of first 10 natural number
+
+x=int(input("Enter how many natural number squares you want:"))
+i=1
+while i<=x:
+    print(i**2,end=' ')
+    i+=1    
