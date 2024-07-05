347fdb9eec4e0ceba1795a70666bd346bf56900a
Asif Ali Sahaji <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2024-07-05 14:36:06 +0530

    Create Qn-14.py

---
+++ while loop/Qn-14.py
@@ -0,0 +1,7 @@
+#write a python script to print cubes of first 10 natural number
+x=int(input("Enter how many multiple of 5 you want to print:"))
+i=1
+while i<=x:
+    print(5*i,end=' ') 
+    i+=1
+
