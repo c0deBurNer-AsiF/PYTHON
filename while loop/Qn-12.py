bd06675c2a3d2d5384dfc3c5c0a3136662c98121
Asif Ali Sahaji <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2024-07-05 14:20:23 +0530

    Create Qn-12.py

---
+++ while loop/Qn-12.py
@@ -0,0 +1,7 @@
+#write a python script to print cubes of first 10 natural number
+
+x=int(input("Enter how many natural number squares you want:"))
+i=1
+while i<=x:
+    print(i**3,end=' ')
+    i+=1         
