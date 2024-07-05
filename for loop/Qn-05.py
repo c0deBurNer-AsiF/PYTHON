6ab324332f9e594ad01e4b231023d6c2b3fe420a
Asif Ali Sahaji <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2024-07-05 23:31:10 +0530

    Create Qn-05.py

---
+++ for loop/Qn-05.py
@@ -0,0 +1,11 @@
+#count number of digit in a given number:
+
+x,count=input("Enter  a number:"),0
+for i in x:
+    count+=1  
+print("Total number of digit is-->",count)
+
+#2nd method--->
+'''
+print("Number of digit-->",len(input("Enter a string:")))
+'''
