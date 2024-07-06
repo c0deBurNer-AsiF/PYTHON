ad764926511bc8ceba499c6913cbb142e0b9d5b0
Asif Ali Sahaji <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2024-07-06 23:25:54 +0530

    Update Qn-10.py

--- List/Qn-10.py
+++ List/Qn-10.py
@@ -1,5 +1,15 @@
 #sort list elements in descending order!
+
+l=[int(e) for e in input("Enter numbers seprated by comma:").split(",")]
+l.sort(reverse=True)
+print(l)
+
+
+#2nd method--->
+
+'''
 l=[int(e) for e in input("Enter number seprated by comma:").split(",")]
 l.sort()
 l.reverse()
 print(l)
+'''
