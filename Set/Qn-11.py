4c474a82823ef720391006b935505c90f438fadd
Asif Ali Sahaji <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2024-07-10 01:40:37 +0530

    Create Qn-11.py

---
+++ Set/Qn-11.py
@@ -0,0 +1,4 @@
+#write a pyhton script to print all distict element of a list.
+# use set to solve the problem
+
+print("Distict elements are:",set([eval(e) for e in input("Enter integers seprated by comma:").split(",")]))
