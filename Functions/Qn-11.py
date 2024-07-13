ec4bbbacc547ce19c82431f1a13964fcca5ac3ea
Asif Ali Sahaji <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2024-07-13 10:39:39 +0530

    Create Qn-11.py

---
+++ Functions/Qn-11.py
@@ -0,0 +1,16 @@
+#sum of two number using python(TSRS)
+
+def sum(x,y):
+    return "sum of {} and {} is {}".format(x,y,x+y)
+
+   
+
+
+
+#2nd method--->
+'''
+def sum(x,y):
+    return "sum of {} and {} is {}".format(x,y,x+y)
+
+s=print(sum(int(input("Enter 1st number:")),int(input("Enter second number:"))))
+'''
