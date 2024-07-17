fc950a20a2894a785e8571f514529ac7b6e11f6a
Asif Ali Sahaji <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2024-07-18 00:41:34 +0530

    Create Qn-16.py

---
+++ Functions/Qn-16.py
@@ -0,0 +1,15 @@
+#check if a number is even(TSRS)
+
+def check_even(n):
+    return print("Its a even number" if n%2==0 else "Its not a even number")
+
+
+
+#2nd method--->
+
+'''
+def check_even(n):
+    return print("Its a even number" if n%2==0 else "Its not a even number")
+
+result=check_even(int(input("Enter a number:")))
+'''
