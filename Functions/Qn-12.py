e64e7bc38b16f8f9587a62f3879a76219536efef
Asif Ali Sahaji <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2024-07-17 14:05:30 +0530

    Create Qn-12.py

---
+++ Functions/Qn-12.py
@@ -0,0 +1,14 @@
+#calculate area of circle(TSRS)
+
+def area_circle(r):
+    return "Area of circle is {}".format(3.14*(r)**2)
+
+
+
+#2nd method--->
+'''
+def area_circle(r):
+    return "Area of circle is {}".format(3.14*(r)**2)
+area=print(area_circle(int(input("Enter the redius:"))))
+
+'''
