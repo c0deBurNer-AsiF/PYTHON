db32f6a6ba725f0b6ebf00d42f54226f8e761a2e
Asif Ali Sahaji <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2024-07-18 01:17:39 +0530

    Create Qn-21.py

---
+++ Functions/Qn-21.py
@@ -0,0 +1,16 @@
+#print first N odd natural numbers(TSRN)
+
+def nOdd(n):
+    for i in range(1,n+1):
+        print(i*2-1,end=" ")
+
+
+
+
+#2nd method--->
+'''
+def nOdd(n):
+    for i in range(1,n+1):
+        print(i*2-1,end=" ")
+nOdd(int(input("Enter how many odd numbers you want to print:")))
+'''
