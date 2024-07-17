91142df3c8e24c4451e775d63ec946d3f8f16961
Asif Ali Sahaji <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2024-07-17 14:08:26 +0530

    Create Qn-15.py

---
+++ Functions/Qn-15.py
@@ -0,0 +1,14 @@
+#calculate compound interest(TSRS)
+
+def compoundInterest(p,r,t):
+    return print("Compound interest is",(p*(1+r/100)**t)-p)
+
+
+
+
+#2nd method--->
+'''
+def compoundInterest(p,r,t):
+    return print("Compound interest is",(p*(1+r/100)**t)-p)
+result=compoundInterest(1000,3.5,1)
+'''
