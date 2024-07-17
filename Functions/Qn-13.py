38e8d20b7491ec556d6b302e9e89e1688aaf6b68
Asif Ali Sahaji <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2024-07-17 14:06:15 +0530

    Create Qn-13.py

---
+++ Functions/Qn-13.py
@@ -0,0 +1,13 @@
+#average of three numbers (TSRS)
+
+def avg(a,b,c):
+    return 'Avg of {},{} and {} is {}'.format(a,b,c,(a+b+c)/3)
+
+#2nd method-->
+
+'''
+def avg(a,b,c):
+    return 'Avg of {},{} and {} is {}'.format(a,b,c,(a+b+c)/3)
+
+result=print(avg(10,20,30))
+'''
