6af0dcb3fb64f177f5cd811a4d39ffbc6efcffa5
Asif Ali Sahaji <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2024-07-17 14:07:04 +0530

    Create Qn-14.py

---
+++ Functions/Qn-14.py
@@ -0,0 +1,12 @@
+#volume of cuboid(TSRS)
+
+def volume_cuboid(h,l,w):
+    return print('Volume of cubiod is',h*l*w)
+
+#2nd method-->
+'''
+def volume_cuboid(h,l,w):
+    return print('Volume of cubiod is',h*l*w)
+
+result=volume_cuboid(5,12,4)
+'''
