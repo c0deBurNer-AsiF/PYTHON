c949c0f9312de90422b2409495c53906b774ffa4
Asif Ali Sahaji <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2024-07-27 14:29:14 +0530

    Create Qn-02.py

---
+++ Map, Reduce and Filter/Qn-02.py
@@ -0,0 +1,7 @@
+#write a python script to find number of digits in each of the elements in a given tuple of numbers
+# Use map function
+
+
+def length_of_digits(x):
+    return int(len(x))
+#print(tuple(map(length_of_digits,(e for e in input("Enter numbers seprated by comma:").split(",")))))
