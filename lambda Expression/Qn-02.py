711964e2b3a906d857ce8b6941c2c56fdb09f65f
Asif Ali Sahaji <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2024-08-11 23:15:50 +0530

    Create Qn-02.py

---
+++ lambda Expression/Qn-02.py
@@ -0,0 +1,10 @@
+#write a lambda expression to calculate area of circle
+
+(lambda n:print(3.14*n**2))(eval(input("Enter the value of radius:")))
+
+#2nd method--->
+
+
+
+f=(lambda n:3.14*n**2)
+f(5)
